8819c8c97ca3c3537d433f2f1a4459ce071887db
Christiaan B van Zyl <christiaanvanzyl@gmail.com>
2025-09-05 11:17:36 +0200

    Working examples

--- timetracker.lua
+++ timetracker.lua
@@ -1,6 +1,6 @@
 VERSION = "0.1.0"
 
--- See definition of import function
+-- Link to definition of import function
 -- https://github.com/zyedidia/micro/blob/master/cmd/micro/initlua.go#L22
 local micro = import("micro")
 local config = import("micro/config")
@@ -8,75 +8,75 @@ local buffer = import("micro/buffer")
 
 -- Example code copied from https://github.com/NicolaiSoeborg/manipulator-plugin
 --
--- -- Returns Loc-tuple w/ current marked text or whole line (begin, end)
--- function getTextLoc(c)
---     local v = micro.CurPane()
---     local a, b = nil, nil
---     if c:HasSelection() then
---         if c.CurSelection[1]:GreaterThan(-c.CurSelection[2]) then
---             a, b = c.CurSelection[2], c.CurSelection[1]
---         else
---             a, b = c.CurSelection[1], c.CurSelection[2]
---         end
---     else
---         local eol = string.len(v.Buf:Line(c.Loc.Y))
---         a, b = c.Loc, buffer.Loc(eol, c.Y)
---     end
---     return buffer.Loc(a.X, a.Y), buffer.Loc(b.X, b.Y)
--- end
+-- Returns Loc-tuple w/ current marked text or whole line (begin, end)
+function getTextLoc(c)
+    local v = micro.CurPane()
+    local a, b = nil, nil
+    if c:HasSelection() then
+        if c.CurSelection[1]:GreaterThan(-c.CurSelection[2]) then
+            a, b = c.CurSelection[2], c.CurSelection[1]
+        else
+            a, b = c.CurSelection[1], c.CurSelection[2]
+        end
+    else
+        local eol = string.len(v.Buf:Line(c.Loc.Y))
+        a, b = c.Loc, buffer.Loc(eol, c.Y)
+    end
+    return buffer.Loc(a.X, a.Y), buffer.Loc(b.X, b.Y)
+end
 
 -- -- Returns the current marked text or whole line
--- function getText(a, b)
---     local txt, buf = {}, micro.CurPane().Buf
+function getText(a, b)
+    local txt, buf = {}, micro.CurPane().Buf
 
---     -- Editing a single line?
---     if a.Y == b.Y then
---         return buf:Line(a.Y):sub(a.X+1, b.X)
---     end
+    -- Editing a single line?
+    if a.Y == b.Y then
+        return buf:Line(a.Y):sub(a.X+1, b.X)
+    end
 
---     -- Add first part of text selection (a.X+1 as Lua is 1-indexed)
---     table.insert(txt, buf:Line(a.Y):sub(a.X+1))
+    -- Add first part of text selection (a.X+1 as Lua is 1-indexed)
+    table.insert(txt, buf:Line(a.Y):sub(a.X+1))
 
---     -- Stuff in the middle
---     for lineNo = a.Y+1, b.Y-1 do
---         table.insert(txt, buf:Line(lineNo))
---     end
+    -- Stuff in the middle
+    for lineNo = a.Y+1, b.Y-1 do
+        table.insert(txt, buf:Line(lineNo))
+    end
 
---     -- Insert last part of selection
---     table.insert(txt, buf:Line(b.Y):sub(1, b.X))
+    -- Insert last part of selection
+    table.insert(txt, buf:Line(b.Y):sub(1, b.X))
 
---     return table.concat(txt, "\n")
--- end
+    return table.concat(txt, "\n")
+end
 
--- -- Calls 'manipulator'-function on text matching 'regex'
--- function manipulate(regex, manipulator, num)
---   local num = math.inf or num
---   local v = micro.CurPane()
---   -- perform the replacement for each cursor
---   local cs = v.Buf:GetCursors()
---   for i=1, #cs do
---     local c = cs[i]
---     local a, b = getTextLoc(c)
---     local oldTxt = getText(a,b)
---     local newTxt = string.gsub(oldTxt, regex, manipulator, num)
+-- Calls 'manipulator'-function on text matching 'regex'
+function manipulate(regex, manipulator, num)
+  local num = math.inf or num
+  local v = micro.CurPane()
+  -- perform the replacement for each cursor
+  local cs = v.Buf:GetCursors()
+  for i=1, #cs do
+    local c = cs[i]
+    local a, b = getTextLoc(c)
+    local oldTxt = getText(a,b)
+    local newTxt = string.gsub(oldTxt, regex, manipulator, num)
 
---     v.Buf:Replace(a, b, newTxt)
+    v.Buf:Replace(a, b, newTxt)
 
---     -- Fix selection, if transformation changes text length (e.g. base64)
---     local d = string.len(newTxt) - string.len(oldTxt)
---     if d ~= 0 then
---         if c:HasSelection() then
---             if c.CurSelection[1]:GreaterThan(-c.CurSelection[2]) then
---                 c.CurSelection[1].X = c.CurSelection[1].X - d
---             else
---                 c.CurSelection[2].X = c.CurSelection[2].X + d
---             end
---         end
---       end
---       --c:Relocate()
---       --c.LastVisualX = v.Cursor:GetVisualX()
---   end
--- end
+    -- Fix selection, if transformation changes text length (e.g. base64)
+    local d = string.len(newTxt) - string.len(oldTxt)
+    if d ~= 0 then
+        if c:HasSelection() then
+            if c.CurSelection[1]:GreaterThan(-c.CurSelection[2]) then
+                c.CurSelection[1].X = c.CurSelection[1].X - d
+            else
+                c.CurSelection[2].X = c.CurSelection[2].X + d
+            end
+        end
+      end
+      --c:Relocate()
+      --c.LastVisualX = v.Cursor:GetVisualX()
+  end
+end
 
 
 -- --[[ DEFINE FUNCTIONS ]]--
@@ -136,12 +136,12 @@ local buffer = import("micro/buffer")
 --     end
 -- ) end
 
--- -- Credit: http://lua-users.org/wiki/StringRecipes
--- function capital() manipulate("(%a)([%w_']*)",
---     function (first, rest)
---         return first:upper() .. rest:lower()
---     end
--- ) end
+-- Credit: http://lua-users.org/wiki/StringRecipes
+function capital() manipulate("(%a)([%w_']*)",
+    function (first, rest)
+        return first:upper() .. rest:lower()
+    end
+) end
 
 -- function title() manipulate(".*",
 --     function (data)
@@ -162,17 +162,17 @@ local buffer = import("micro/buffer")
 --     end
 -- ) end
 
--- function kebab() manipulate(".*",
---     function (data)
---         local function parseChar(char)
---             if char:match("%u") then return "-"..char:lower() end
---             if char:match("%s") then return "" end
---             return char:lower()
---         end
+function kebab() manipulate(".*",
+    function (data)
+        local function parseChar(char)
+            if char:match("%u") then return "-"..char:lower() end
+            if char:match("%s") then return "" end
+            return char:lower()
+        end
 
---         return data:gsub("^%a", string.lower):gsub(".", parseChar):gsub("%s", "-"):gsub("_", "-")
---     end
--- ) end
+        return data:gsub("^%a", string.lower):gsub(".", parseChar):gsub("%s", "-"):gsub("_", "-")
+    end
+) end
 
 -- function snake() manipulate(".*",
 --     function (data)
@@ -198,8 +198,8 @@ local buffer = import("micro/buffer")
 --     end
 -- ) end
 
--- function init()
---     config.MakeCommand("capital", capital, config.NoComplete)
+function init()
+    config.MakeCommand("capital", capital, config.NoComplete)
 --     -- Thanks marinopposite
 --     config.MakeCommand("brace", function() manipulate(".*", "(%1)", 1) end, config.NoComplete)
 --     config.MakeCommand("curly", function() manipulate(".*", "{%1}", 1) end, config.NoComplete)
@@ -214,19 +214,16 @@ local buffer = import("micro/buffer")
 --     config.MakeCommand("incNum", incNum, config.NoComplete)
 --     config.MakeCommand("rot13", rot13, config.NoComplete)
 --     config.MakeCommand("upper", function() manipulate("[%a]", string.upper) end, config.NoComplete)
---     config.MakeCommand("lower", function() manipulate("[%a]", string.lower) end, config.NoComplete)
+    config.MakeCommand("lower", function() manipulate("[%a]", string.lower) end, config.NoComplete)
 --     config.MakeCommand("reverse", function() manipulate(".*", string.reverse) end, config.NoComplete)
 --     config.MakeCommand("title", title, config.NoComplete)
 --     config.MakeCommand("pascal", pascal, config.NoComplete)
 --     config.MakeCommand("camel", camel, config.NoComplete)
---     config.MakeCommand("kebab", kebab, config.NoComplete)
+    config.MakeCommand("kebab", kebab, config.NoComplete)
 --     config.MakeCommand("snake", snake, config.NoComplete)
 --     config.MakeCommand("screamingSnake", screamingSnake, config.NoComplete)
 
+-- TODO Function to insert current date at cursor position and key binding
 
---     config.AddRuntimeFile("manipulator", config.RTHelp, "help/manipulator.md")
--- end
-
-function init()
-  -- TODO Function to insert current date at cursor position
+    config.AddRuntimeFile("manipulator", config.RTHelp, "help/manipulator.md")
 end
